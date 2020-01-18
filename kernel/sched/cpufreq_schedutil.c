3da104a80cd2fef44617de852959840504d5ace9
Ethan Halsall <ethan.halsall@gmail.com>
2020-01-18 22:31:37 +0000

    schedutil: add likely to all use_pelt()
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -174,7 +174,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 
 		policy->cur = next_freq;
 	} else {
-		if (use_pelt())
+		if (likely(use_pelt()))
 			sg_policy->work_in_progress = true;
 		sched_irq_work_queue(&sg_policy->irq_work);
 	}
@@ -453,7 +453,7 @@ static void sugov_work(struct kthread_work *work)
 #endif
 	mutex_unlock(&sg_policy->work_lock);
 
-	if (use_pelt())
+	if (likely(use_pelt()))
 		sg_policy->work_in_progress = false;
 }
 
